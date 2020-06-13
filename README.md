c7d0f9a057a5f4bbb74ab553aaa1b89f7c4c4403
AslakOffi <51925794+AslakOffi@users.noreply.github.com>
2020-06-13 22:49:17 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+#Français
+
+# Utilité
+VerifID est un NIB (Numéro d'identification bancaire) mis en place via l'api : Bincodes.com
+
+# Pré-requis
+npm i discord.js<br/>
+npm i colors<br/>
+npm i colors.js<br/>
+npm i request<br/>
+
+# Utilisation
+__Pour utiliser ce code, il vous suffit de faire le prefix de votre bot (ici $) plus "verif"__<br/><br/>Exemple :<br/>
+$verif 624680<br/><br/>
+La réponse donnée sera donc :<br/>
+
+# English
+
+# Usefulness
+VerifID is a BIN (Bank Identification Number) set up via the API: Bincodes.com
+
+# Pre-requisites
+npm i discord.js <br/>
+npm i colors <br/>
+npm i colors.js <br/>
+npm i request <br/>
+
+# Use
+__To use this code, just make the prefix of your bot (here $) more "verif"__<br/><br/>Example: <br/>
+$verif 624680 <br/><br/>
+The answer given will therefore be: <br/>
---
+++ VerifID.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const colors = require('colors');
+const request = require('request');
+
+
+const config = require('./config.json');
+const key = config.api_key;
+const token = config.token;
+const prefix = config.prefix;
+
+// Démarrage du bot.
+
+client.on("ready", function() {
+    console.log("[".white + "+".cyan + "]".white + " -".cyan + " VerifID V1.0 crée et développé par".white + " Aslak#001".cyan + ".".white);
+    console.log("[".white + "+".cyan + "]".white + " -".cyan + " Le vérificateur utilise l'api bincode (".white + "https://bincodes.com/".cyan + ")".white + ".".white);
+});
+
+
+client.on("message", function(m) {
+    if(message.channel.type !== "text") return;
+    var args = message.content.split(/ +/);
+
+    switch(args[0]) {
+
+        // Lorsque l'utilisateur exécute la commande...
+
+        case `${prefix}verif`:
+            if(message.deletable) m.delete()
+            if(!args[1]) {
+
+                // Si le NIB n'est pas valide...
+
+                message.channel.send(":x: | Merci de bien vouloir préciser un numéro d'identification bancaire valide.\n__**Exemple**__ : " + prefix + "verif 624680")
+                return;
+            } else {
+
+                // Si le NIB est valide alors envoyé le résultat.
+
+                if(message.deletable) message.delete()
+                request(`http://api.bincodes.com/bin/?format=json&api_key=${key}&bin=${args[1]}`, function(error, response, body) {
+                    if(!error && response.statusCode === 200) {
+                        let page = JSON.parse(body);
+                        message.channel.send("> N°ID Bancaire : " + args[1] + "\n> Banque : " + page.bank + "\n> Marque : " + page.card + "\n> Type : " + page.type + "\n> Niveau : " + page.level + "\n> Pays : " + page.country)
+                    }
+                })
+            }
+            break;
+
+    };
+
+});
+
+// Veuillez entrer le token de votre bot à la place de "token".
+
+client.login(token);
